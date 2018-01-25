df0d1a91a8866c9f924150a08c1f844236ab5fc0
engswee <engswee+git@gmail.com>
2018-01-25 15:06:03 +0800

    Initial version of Integration Flow template
    Consists of:-
    Sample Groovy script (default CPI script)
    Sample Groovy testing script
    Sample Spock specification
    Sample Integration Flow

---
+++ com.equalize.cpi.iflow.template/test/com/equalize/cpi/spock/testing/GroovyScriptSpecification.groovy
@@ -0,0 +1,39 @@
+package com.equalize.cpi.spock.testing
+
+import com.sap.gateway.ip.core.customdev.processor.MessageImpl
+import com.sap.gateway.ip.core.customdev.util.Message
+import spock.lang.Shared
+
+class GroovyScriptSpecification extends spock.lang.Specification {
+
+	@Shared GroovyShell shell = new GroovyShell()
+	@Shared Script script
+	private Message msg
+	
+	def setupSpec() {
+		// Load Groovy Script		
+		script = shell.parse(new File("src/main/resources/script/default.groovy"))
+	}
+	
+	def setup() {
+		this.msg = new MessageImpl()
+	}
+	
+	def "message body, headers and properties are updated "() {
+		
+		given: "the message body, header and propery is provided with initial value"
+		this.msg.setBody(new String("Hello Groovy World"))
+		this.msg.setHeader("oldHeader", "MyGroovyHeader")
+		this.msg.setProperty("oldProperty", "MyGroovyProperty")
+		
+		when: "we execute the Groovy script"
+		script.processData(this.msg)
+		
+		then: "the body, headers and properties are correctly populated or modified"
+		this.msg.getBody() == "Hello Groovy WorldBody is modified"
+		this.msg.getHeaders()["oldHeader"] == "MyGroovyHeadermodified"
+		this.msg.getHeaders()["newHeader"] == "newHeader"
+		this.msg.getProperties()["oldProperty"] == "MyGroovyPropertymodified"
+		this.msg.getProperties()["newProperty"] == "newProperty"
+	}
+}
