0603eab8fead0a9bc612f8dbb51e8a7e505b8063
engswee <engswee+git@gmail.com>
2018-06-28 13:59:58 +0800

    Update ScriptTester with Camel Automatic Type Converter

--- com.equalize.cpi.iflow.template/test/main/resources/script/GroovyScriptTester.groovy
+++ com.equalize.cpi.iflow.template/test/main/resources/script/GroovyScriptTester.groovy
@@ -1,21 +1,39 @@
 import com.sap.gateway.ip.core.customdev.processor.MessageImpl
 import com.sap.gateway.ip.core.customdev.util.Message
+import org.apache.camel.CamelContext
+import org.apache.camel.Exchange
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.impl.DefaultExchange
+
 // Load Groovy Script
 GroovyShell shell = new GroovyShell()
-def script = shell.parse(new File("src/main/resources/script/default.groovy"))
+def script = shell.parse(new File('src/main/resources/script/default.groovy'))
+
+// Setup Camel context and exchange for the message
+CamelContext context = new DefaultCamelContext()
+Exchange exchange = new DefaultExchange(context)
+Message msg = new MessageImpl(exchange)
+
+//--------------------------------------------------------------
 // Initialize message with body, header and property
-Message msg = new MessageImpl()
-msg.setBody(new String("Hello Groovy World"))
-msg.setHeader("oldHeader", "MyGroovyHeader")
-msg.setProperty("oldProperty", "MyGroovyProperty")
+def body = new String('Hello Groovy World')
+msg.setHeader('oldHeader', 'MyGroovyHeader')
+msg.setProperty('oldProperty', 'MyGroovyProperty')
+//--------------------------------------------------------------
+
+// Set exchange body in case automatic Type Conversion is required
+exchange.getIn().setBody(body)
+msg.setBody(exchange.getIn().getBody())
+
 // Execute script
 script.processData(msg)
+
 // Display results of script in console
-println("Body:\r\n" + msg.getBody())
+println("Body:\r\n${msg.getBody()}")
 
 def displayMaps = { String mapName, Map map ->
 	println mapName
 	map.each { key, value -> println("$key = $value") }
 }
-displayMaps("Headers:", msg.getHeaders())
-displayMaps("Properties:", msg.getProperties())
+displayMaps('Headers:', msg.getHeaders())
+displayMaps('Properties:', msg.getProperties())
